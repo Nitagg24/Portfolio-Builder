fd10d248dc1f65216a55ebb3b79f0cad922e4179
nitin aggarwal <agg.nitin124@gmail.com>
2024-09-02 01:53:45 +0530

    Project Completed

--- app/[username]/_components/UserDetailInfo.jsx
+++ app/[username]/_components/UserDetailInfo.jsx
@@ -1,35 +1,26 @@
 "use client"
-import { TwicPicture } from '@twicpics/components/react'
-import { MapPin, Share } from 'lucide-react'
-import React from 'react'
+import { TwicPicture } from '@twicpics/components/react';
+import { MapPin } from 'lucide-react';
+import React from 'react';
 
-function UserDetailInfo({userDetail}) {
-  return userDetail?.name&&(
-    <div className='flex flex-col md:justify-center  md:h-screen overflow-hidden'>
-        <div className='w-full flex items-center justify-between'>
-            <div className='flex md:flex-col items-center md:items-start gap-4'>
-                <TwicPicture src={userDetail?.profileImage}
-                    className='w-[70px] h-[70px] md:w-[130px] md:h-[130px] rounded-full'
-                />
-                <div className='flex flex-col gap-2 lg:gap-4 mt-3'>
-                    <h2 className='font-bold text-lg md:text-2xl'>{userDetail.name}</h2>
-                    <h2 className='flex gap-2 text-sm md:text-md items-center text-gray-500'> <MapPin/> {userDetail.location}</h2>
-                    <div>
-                    <button className='md:hidden btn btn-primary btn-sm'>
-                        <Share className='h-3 w-3'/> Share</button>
-                </div>
-                </div>
-                
-            </div>
-            
-        </div>
-        <h2 className='my-7 '>{userDetail.bio}</h2>
-        <div className='flex gap-2'>
+function UserDetailInfo({ userDetail }) {
+  return userDetail?.name && (
+    <div className='flex flex-col items-center justify-center h-auto py-10 lg:h-screen md:py-0'>
+      <TwicPicture src={userDetail?.profileImage}
+        className='w-32 h-32 md:w-48 md:h-48 rounded-full'
+      />
+      <h2 className='font-bold text-lg md:text-2xl mt-4 mb-1'>{userDetail.name}</h2>
+      <h2 className='flex gap-2 text-sm md:text-md items-center text-gray-500'>
+        <MapPin className='w-4 h-4' /> {userDetail.location}
+      </h2>
+      <p className='text-center mt-2 mb-4'>{userDetail.bio}</p>
+      <div className='flex justify-center items-center gap-2 w-full px-4'>
         <input type="text" placeholder="Add your email" className="input input-bordered w-full max-w-xs" />
         <button className='btn btn-primary'>Subscribe</button>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserDetailInfo
+export default UserDetailInfo;
+
--- app/[username]/page.jsx
+++ app/[username]/page.jsx
@@ -1,24 +1,22 @@
 "use client"
-import React, { useContext } from 'react'
-import UserDetailInfo from './_components/UserDetailInfo'
-import ProjectList from './_components/ProjectList'
-import { UserDetailContext } from '../_context/UserDetailContext'
+import React, { useContext } from 'react';
+import UserDetailInfo from './_components/UserDetailInfo';
+import ProjectList from './_components/ProjectList';
+import { UserDetailContext } from '../_context/UserDetailContext';
 
 function UserPage() {
-  const {userDetail,setUserDetail}=useContext(UserDetailContext);
- 
+  const { userDetail } = useContext(UserDetailContext);
+
   return (
-    <div className='p-3 lg:px-20
-    grid grid-cols-1 lg:grid-cols-3 gap-5 overflow-hidden'>
+    <div className='p-3 lg:px-20 grid grid-cols-1 lg:grid-cols-3 gap-5 overflow-hidden'>
       <div className=''>
         <UserDetailInfo userDetail={userDetail} />
       </div>
-      <div className='lg:col-span-2'>
-        
+      <div className='lg:ml-1/3 lg:col-span-2'>
         <ProjectList projectList={userDetail?.project} />
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default UserPage
+export default UserPage;
